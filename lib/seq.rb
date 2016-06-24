a344eb543477ec078ac337be977b950fa9e92375
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-06-24 17:16:38 +0000

    successfully completed iterative method using TDD

--- lib/seq.rb
+++ lib/seq.rb
@@ -4,15 +4,25 @@ class Sequence
 	# allows me access input value of @n
 	attr_accessor :n
 	# initializer sets up variables to be referenced in the different functions
-	def initialize(n)
-		@n = n
+	def initialize(upper_index)
+		@upper_index = upper_index
 	end
 
 	# iterative_fib will build a fibonacci sequence within an array
 	def iterative_fib
+		# create an array to store resulting sequence
+		result = []
 		# fib(0) = 0
-		
+		result << 0
 		# fib(1) = 1
-		# fib(n)= fib(n-1) + fib(n-2)
+		result << 1
+		# fib(n) = fib(n-1) + fib(n-2)
+		for i in 2..@upper_index do
+			result << (result[i - 1] + result[i - 2])
+		end
+		result
 	end
 end
+
+#seq = Sequence.new(9)
+#seq.iterative_fib
--- spec/seq_spec.rb
+++ spec/seq_spec.rb
@@ -1,22 +1,21 @@
 require 'spec_helper'
 
-RSpec.describe Sequence, type: :model do
+RSpec.describe Sequence, type: :mode do
 	describe "should output fibonacci sequence" do 
 		it "should output fibonacci sequence using iterative method" do 
 			n = 9
 			expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-			
 
-			sequence = Sequence.new(n)
-			expect(sequence.interative_fib.result).to eq expected
+			seq = Sequence.new(n)
+			expect(seq.iterative_fib).to eq expected
 		end
 
 		it "should output fibonacci sequence using recursive method" do
 			n = 9
 			expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-			sequence = Sequence.new(n)
-			expect(sequence.recursive_fib.result).to eq expected
+			seq = Sequence.new(n)
+			expect(seq.recursive_fib).to eq expected
 		end
 	end
 end
